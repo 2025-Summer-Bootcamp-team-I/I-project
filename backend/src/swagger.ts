5f8fb5904f9a38ed772b1e6f0e28244d3e51817b
ksm0520 <kimseungmin@gmail.com>
2025-07-02 20:54:37 +0900

    chore:프로젝트 ci 세팅

---
+++ backend/src/swagger.ts
@@ -0,0 +1,21 @@
+// @ts-expect-error: no type declaration
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API Docs',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
