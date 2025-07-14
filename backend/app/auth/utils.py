f1f18610461ea8558a9261495ba3d78f7f49ec7e
johe00123 <kita92666@gmail.com>
2025-07-14 22:11:02 +0900

    feat:회원가입 및 로그인 기능 구현#35

--- backend/app/auth/utils.py
+++ backend/app/auth/utils.py
@@ -1,6 +1,12 @@
+from fastapi import Depends, HTTPException, status
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from app.auth import models
+from app.database import get_db
 
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
@@ -19,3 +25,19 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+oauth2_scheme = HTTPBearer()
+
+def get_current_user(token: HTTPAuthorizationCredentials = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token.credentials, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
