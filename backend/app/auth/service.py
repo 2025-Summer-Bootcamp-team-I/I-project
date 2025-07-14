d2f6d4a1a1fc05c644e8d6a605302e6faa8ea3af
johe00123 <kita92666@gmail.com>
2025-07-14 22:10:52 +0900

    feat:회원가입 및 로그인 기능 구현#35

--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -7,7 +7,7 @@ def signup_user(db, user_create):
         raise HTTPException(status_code=400, detail="이미 존재하는 유저명입니다.")
     hashed_pw = utils.get_password_hash(user_create.password)
     crud.create_user(db, user_create.username, hashed_pw)
-    return {"msg": "회원가입 성공! 환영합니다 :)"}
+    return {"msg": "회원가입 성공! 환영합니다~"}
 
 def login_user(db, user_login):
     user = crud.get_user_by_username(db, user_login.username)
