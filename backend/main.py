b71cfb61f67e5c2434371548176fee47026c913e
jiwon <2271388@hansung.ac.kr>
2025-07-14 23:18:10 +0900

    충돌 수정

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-from app.auth import models as auth_models
 from app import database
-from app.auth import api as auth_api
 from app.ad8.api import router as ad8_router   # <-- 수정!
 from app.ad8 import models as ad8_models
 
@@ -13,7 +11,6 @@ ad8_models.Base.metadata.create_all(bind=database.engine)
 app = FastAPI()
 
 # 라우터 등록
-app.include_router(auth_api.router, prefix="/user", tags=["User"])
 app.include_router(ad8_router, prefix="/ad8", tags=["AD8"])
 
 @app.get("/")
