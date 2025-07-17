891eb00d761dcd9085f31c2c698fe2719bc79ec9
jiwon <2271388@hansung.ac.kr>
2025-07-17 14:53:05 +0900

    chore(서버): FastAPI 앱 실행 및 라우터 등록 #66

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,27 @@ from fastapi import FastAPI
 from app.auth import models
 from app import database
 from app.auth import api
+from app.report import api as report_api
 
 # DB 테이블 생성
 models.Base.metadata.create_all(bind=database.engine)
 
 # FastAPI 인스턴스
-app = FastAPI()
+app = FastAPI(
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
 
 # 라우터 등록
 app.include_router(api.router, prefix="/user", tags=["User"])
+app.include_router(report_api.router)
 
 @app.get("/")
 def root():
     return {"msg": "API 서버는 현재 작동 중입니다!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
