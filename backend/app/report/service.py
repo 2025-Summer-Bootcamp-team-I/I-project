6d2e78404078c9419fd779073994b2245157cf71
jiwon <2271388@hansung.ac.kr>
2025-07-18 02:30:15 +0900

    fix(Report): 빈 리포트 생성을 위한 서비스 로직 구현 #106

--- backend/app/report/service.py
+++ backend/app/report/service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from . import crud, schemas
 
-def create_empty_report_service(db: Session, report: schemas.ReportCreate):
+def create_report_service(db: Session, report: schemas.ReportCreate):
     # 모든 필드를 명시적으로 전달
     empty_report_data = schemas.ReportCreate(
         user_id=report.user_id,
@@ -9,17 +9,7 @@ def create_empty_report_service(db: Session, report: schemas.ReportCreate):
         chat_result="",
         ad8test_result="",
         soundtest_result="",
-        final_result="",
         recommendation="",
-        total_score=0,
-        sound_score=0,
-        ad8_score=0,
-        drawing_score=0,
-        text_score=0,
-        memory_score=0,
-        time_space_score=0,
-        judgment_score=0,
-        visual_score=0,
-        language_score=0
+        total_score=0
     )
-    return crud.create_empty_report(db, empty_report_data)
+    return crud.create_report(db, empty_report_data)
