63aef7d2515be3ba4e6d81760291b70546ad87f7
jiwon <2271388@hansung.ac.kr>
2025-07-18 02:27:18 +0900

    fix(AD8): AD8Test 및 AD8Response 모델 정의 #106

--- backend/app/ad8/models.py
+++ backend/app/ad8/models.py
@@ -4,15 +4,15 @@ from app.database import Base
 class AD8Test(Base):
     __tablename__ = "ad8_test"
 
-    ad8test_id = Column(Integer, primary_key=True, index=True)
-    # report_id = Column(Integer, ForeignKey("report.report_id"), nullable=False)
+    AD8Test_id = Column(Integer, primary_key=True, index=True)
+    report_id = Column(Integer, ForeignKey("reports.report_id"), nullable=False)
     risk_score = Column(Integer, nullable=False)
 
 class AD8Response(Base):
     __tablename__ = "ad8_response"
 
     response_id = Column(Integer, primary_key=True, index=True)
-    ad8test_id = Column(Integer, ForeignKey("ad8_test.ad8test_id"), nullable=False)
+    AD8Test_id = Column(Integer, ForeignKey("ad8_test.AD8Test_id"), nullable=False)
     question_no = Column(Integer, nullable=False)
     is_correct = Column(Boolean, nullable=False)
 
