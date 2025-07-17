972f11f50743e96553349568c44dee482e91f617
jiwon <2271388@hansung.ac.kr>
2025-07-17 14:48:13 +0900

    feat(보고서): report API 라우터 작성 #66

---
+++ backend/app/report/api.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from . import schemas, service
+
+router = APIRouter()
+
+@router.post("/reports/empty", response_model=schemas.ReportResponse)
+def create_empty_report(report: schemas.ReportCreate, db: Session = Depends(get_db)):
+    new_report = service.create_empty_report_service(db, report)
+    return new_report
