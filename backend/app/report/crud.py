c6c790cbc24efa6f6bd3d398bb73b17d9ee94560
jiwon <2271388@hansung.ac.kr>
2025-07-18 17:52:21 +0900

    feat(Report): 빈 리포트 생성 로직 구현 #113

--- backend/app/report/crud.py
+++ backend/app/report/crud.py
@@ -8,8 +8,18 @@ def create_empty_report(db: Session, report: schemas.ReportCreate):
         chat_result=report.chat_result,
         ad8test_result=report.ad8test_result,
         soundtest_result=report.soundtest_result,
+        final_result=report.final_result,
         recommendation=report.recommendation,
         total_score=report.total_score,
+        sound_score=report.sound_score,
+        ad8_score=report.ad8_score,
+        drawing_score=report.drawing_score,
+        text_score=report.text_score,
+        memory_score=report.memory_score,
+        time_space_score=report.time_space_score,
+        judgment_score=report.judgment_score,
+        visual_score=report.visual_score,
+        language_score=report.language_score
     )
     db.add(new_report)
     db.commit()
