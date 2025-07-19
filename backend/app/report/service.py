3e980bcd143310a0c083f85415b9f1300082df5c
plar8271 <plar8271@naver.com>
2025-07-19 20:02:44 +0900

    schemas.py 변경에 따른 수정 #126

--- backend/app/report/service.py
+++ backend/app/report/service.py
@@ -1,10 +1,9 @@
 from sqlalchemy.orm import Session
 from . import crud, schemas
 
-def create_empty_report_service(db: Session, report: schemas.ReportCreate):
+def create_empty_report_service(db: Session, user_id: int):
     # 모든 필드를 명시적으로 전달
     empty_report_data = schemas.ReportCreate(
-        user_id=report.user_id,
         drawingtest_result="",
         chat_result="",
         ad8test_result="",
@@ -22,4 +21,4 @@ def create_empty_report_service(db: Session, report: schemas.ReportCreate):
         visual_score=0,
         language_score=0
     )
-    return crud.create_empty_report(db, empty_report_data)
+    return crud.create_empty_report(db, empty_report_data, user_id)
