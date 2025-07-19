92f848b351534747dbe38b7e364be45a84699cb5
plar8271 <plar8271@naver.com>
2025-07-19 19:54:59 +0900

    userId를 직접 받는 대신 accesstoken을 통해 userId를 얻도록 수정 #126

--- backend/app/report/api.py
+++ backend/app/report/api.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
 from . import schemas, service
+from app.auth.utils import get_current_user
+from app.auth import models as auth_models
 
 router = APIRouter()
 
 @router.post("/reports/empty", response_model=schemas.SimpleReportResponse)
-def create_empty_report(report: schemas.ReportCreate, db: Session = Depends(get_db)):
-    new_report = service.create_empty_report_service(db, report)
+def create_empty_report(db: Session = Depends(get_db), current_user: auth_models.User = Depends(get_current_user)):
+    new_report = service.create_empty_report_service(db, current_user.id)
     return new_report
